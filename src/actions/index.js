fe433fddb168f9ddbee6a225eebd5b6f27cb3b00
roger gc <nppcpp@hotmail.com>
2019-04-14 11:55:34 +0200

    solving token problem

--- src/actions/index.js
+++ src/actions/index.js
@@ -34,18 +34,23 @@ export const spotifyReset=()=>
   return action;
 };
 
-const spotifyToken= 'BQDkOAaPHbEHYZ24qAGaCYdZT2ADwrDnIdMnNBFfE5XnI8ptTwhL54wnzDlIS5dbNx0g26bpQ0wNBMwQIH0';
-const spotifyConf=
-{
-  url:'https://api.spotify.com/v1/browse/new-releases',
-  method:'get',
-  headers:
-  {
-    Authorization: 'Bearer ' + spotifyToken
-  }
-};
-
 export const spotifyFetchData= ()=>(dispatch)=>
 {
-  axios(spotifyConf).then(response=>dispatch(spotifySetData(response.data)));
+  axios
+  ({
+    url:'https://react1-api.herokuapp.com/api/v1/access_token',
+    method:'get'
+  }).then(resp=>
+    {
+      axios({
+        url:'https://api.spotify.com/v1/browse/new-releases',
+        method:'get',
+        headers: {
+          Authorization: 'Bearer ' + resp.data.access_token
+        }
+      }).then(resp=>
+        {
+        dispatch(spotifySetData(resp.data));
+      });
+    });
 };
